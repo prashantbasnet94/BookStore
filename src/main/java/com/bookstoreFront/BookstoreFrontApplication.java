cb75e27c4ede8b646a878eae318e1f120db51754
prashant basnet <prashantbasnet94@gmail.com>
2017-12-31 18:10:06 -0600

    adding all files to git repo

---
+++ src/main/java/com/bookstoreFront/BookstoreFrontApplication.java
@@ -0,0 +1,12 @@
+package com.bookstoreFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreFrontApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bookstoreFront/BookstoreFrontApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookstoreFront;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstoreFrontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
